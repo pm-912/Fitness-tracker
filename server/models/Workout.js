9be0644b94607ae5f05c342091a776e2f1800c99
edgyhyeji <153347353+edgyhyeji@users.noreply.github.com>
2024-03-07 16:30:35 -0600

    created User.js and Workout.js models in the server/models

---
+++ server/models/Workout.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const { Schema } = mongoose;
+
+const workoutSchema = new Schema({
+
+    title: {
+        type: String,
+        trim: true,
+        require: true
+    },
+
+    details: {
+        type: String,
+        required: true
+    },
+
+    type: {
+        type: String,
+        required: true
+    },
+
+    duration: {
+        type: String,
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => dateFormat(timestamp)
+    },
+
+    comments: [
+        {
+            commentText: {
+                type: String,
+                required: true,
+                minlength: 1,
+                maxlength: 300
+            },
+
+            commentAuthor: {
+                type: String,
+                required: true
+            },
+
+            createdAt: {
+                type: Date,
+                default: Date.now,
+                get: (timestamp) => dateFormat(timestamp)
+            }
+        }
+    ]
+});
+
+const Workout = model('Workout', workoutSchema);
+
+module.exports = Workout;
