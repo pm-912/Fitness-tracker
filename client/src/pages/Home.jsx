52775a288fac3c5d21ba695b038cfd717b2fa698
Peetah <peterleemartin@gmail.com>
2024-03-08 14:11:30 -0600

    finished Home page, pulling new changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,29 +4,13 @@ import { ALL_WORKOUTS } from '../utils/queries'
 
 
 const Home = () => {
-    const { loading, data } = useQuery(ALL_WORKOUTS);
-    const workouts = data?.allWorkouts || [];
     return (
         <main>
-            <div className="flex-row justify-center">
-                <div
-                    className="col-12 col-md-10 mb-3 p-3"
-                    style={{ border: '1px dotted #1a1a1a' }}
-                >
-                    <ThoughtForm />
-                </div>
-                <div className="col-12 col-md-8 mb-3">
-                    {loading ? (
-                        <div>Loading...</div>
-                    ) : (
-                        <ThoughtList
-                            thoughts={thoughts}
-                            title="Some Feed for Thought(s)..."
-                        />
-                    )}
-                </div>
+            <div className="container">
+                <Workout />
             </div>
         </main>
+    );
+};
 
-    )
-}
+export default Home;
