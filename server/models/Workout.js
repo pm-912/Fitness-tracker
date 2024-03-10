2df6efa9f5aedfc744b246c00b46b1381de3bd4d
edgyhyeji <153347353+edgyhyeji@users.noreply.github.com>
2024-03-09 20:49:31 -0600

    i

--- server/models/Workout.js
+++ server/models/Workout.js
@@ -54,6 +54,6 @@ const workoutSchema = new Schema({
     ]
 });
 
-const Workout = model('Workout', workoutSchema);
+const Workout = mongoose.model('Workout', workoutSchema);
 
 module.exports = Workout;
