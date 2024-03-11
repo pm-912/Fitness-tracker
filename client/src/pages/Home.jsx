a220d9efedddb21b4ceeab5b9480f3117c0d5a08
johria <johria.mehboob@gmail.com>
2024-03-11 14:04:49 -0500

    added to home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import { QUERY_ALL_WORKOUTS } from '../utils/queries'
 import { useState } from 'react';
 
 
+
 const Home = () => {
     // const [workouts, setWorkouts] = useState([])
     const { loading, data } = useQuery(QUERY_ALL_WORKOUTS);
@@ -16,7 +17,8 @@ const Home = () => {
                 <div>Loading...</div>
             ) : (
                 workouts && workouts.map((workout) => (
-                <div key={workout._id} className="container">
+                    
+                <div key={workout._id} className="card" style={{ border: '5px solid #ccc', padding: '10px', margin: '20px', borderRadius: '5px', }} >
                     <Workout key={workout._id} workout={workout} />
                 </div>)
                 ))}
