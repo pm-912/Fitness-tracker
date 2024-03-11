47a8d6e140115b2e17126adbc1efebf5e9e0f6e1
Peetah <peterleemartin@gmail.com>
2024-03-11 18:02:59 -0500

    trying to figure out an issue with a page load not receiving data currently. pulling new content

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,14 +8,14 @@ const Home = () => {
     // const [workouts, setWorkouts] = useState([])
     const { loading, data } = useQuery(QUERY_ALL_WORKOUTS);
     const workouts = data?.allWorkouts || []
-    console.log(workouts)
+    // console.log(workouts)
     return (
         <main>
             <h2>Community Workouts</h2>
             {loading ? (
                 <div>Loading...</div>
             ) : (
-                workouts && workouts.map((workout) => (
+                workouts.map((workout) => (
                 <div key={workout._id} className="container">
                     <Workout key={workout._id} workout={workout} />
                 </div>)
