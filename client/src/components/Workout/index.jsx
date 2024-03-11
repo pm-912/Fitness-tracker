7d98ca10a5a9077f17d9172ea50bd5b5f06b3556
Peetah <peterleemartin@gmail.com>
2024-03-10 21:48:52 -0500

    worked a bit on the Workout component

--- client/src/components/Workout/index.jsx
+++ client/src/components/Workout/index.jsx
@@ -1,4 +1,21 @@
+import { Link } from "react-router-dom";
+
+//import workout model
+//
+
 const Workout = () => {
-    
-    return
+
+    return (
+
+        <div className="">
+            <div className="title">
+                <h2>____'s Workouts</h2>
+            </div>
+            <Link to={`/workouts/${_id}`}>
+                <p></p>
+            </Link>
+        </div>
+    )
 }
+
+export default Workout
